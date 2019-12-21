cbfefecfda35c1ff52288f2f288061610dce4cae
Illia Khurtin <iluhakhurtin@gmail.com>
2019-12-21 21:12:14 +1100

    Project structure template. Removed unused styles.

--- front-end-challenge/src/App.js
+++ front-end-challenge/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import controls from "./data/controls.json";
+import "./App.css";
+import QqtrlControls from "./components/qqtrlControls";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let { data } = controls;
+
+    return (
+      <div className="App">
+        <QqtrlControls data={data} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ front-end-challenge/src/components/qqtrlControls/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class QqtrlControls extends Component {
+  render() {
+    return <h1>QqtrlControl</h1>;
+  }
+}
+
+export default QqtrlControls;
