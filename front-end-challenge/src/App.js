a22f41e5a4cd4baf41eab0a8b35a31f589d356a9
Illia Khurtin <iluhakhurtin@gmail.com>
2019-12-22 00:40:48 +1100

    Changed boilerplate from the defaul to webpack.

--- front-end-challenge/src/App.js
+++ front-end-challenge/src/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import controls from "./data/controls.json";
-import "./App.css";
-import QqtrlControls from "./components/qqtrlControls";
 
 class App extends Component {
   render() {
-    let { data } = controls;
-
-    return (
-      <div className="App">
-        <QqtrlControls data={data} />
-      </div>
-    );
+    return <div>Hello React!</div>;
   }
 }
 
--- front-end-challenge/src/components/qqtrlControls/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class QqtrlControls extends Component {
-  render() {
-    return <h1>QqtrlControl</h1>;
-  }
-}
-
-export default QqtrlControls;
