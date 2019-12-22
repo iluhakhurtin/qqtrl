d1de782ccf1432b17ce79020298dbdcdf0dc8d90
Illia Khurtin <iluhakhurtin@gmail.com>
2019-12-22 17:32:01 +1100

    Init modal dialog

--- front-end-challenge/src/components/error/index.js
+++ front-end-challenge/src/components/error/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import ModalBody from "react-bootstrap/ModalBody";
+import ModalFooter from "react-bootstrap/ModalFooter";
 import styles from "./styles.error.module.css";
 
 class Error extends Component {
   render() {
     let { title, onTryAgain } = this.props;
-    return <div className={styles.container}>error</div>;
+    return (
+      <Modal show={true} dialogClassName={styles.container}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <ModalBody></ModalBody>
+        <ModalFooter>This is the footer</ModalFooter>
+      </Modal>
+    );
   }
 }
 
